dd06004dd3a4433b60db0892f9bcafdc1edc3988
Tatiana Carvajal <taticv159912@gmail.com>
2026-04-23 15:26:05 -0500

    First commit

---
+++ Contacts/Contacts/ContactsApp.swift
@@ -0,0 +1,17 @@
+//
+//  ContactsApp.swift
+//  Contacts
+//
+//  Created by Tatiana Carvajal on 23/04/26.
+//
+
+import SwiftUI
+
+@main
+struct ContactsApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
